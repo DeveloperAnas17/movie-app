f38df77555695a806cce32cce10f3cae15431956
Anas <amanabbasi3660@gmail.com>
2022-04-25 23:36:26 +0530

    ...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { RiMovie2Fill } from "react-icons/ri";
+import { RiMovie2Fill, RiCloseFill } from "react-icons/ri";
+import { FcMenu } from "react-icons/fc";
 
 const headerNav = [
   {
@@ -18,29 +19,59 @@ const headerNav = [
 ];
 
 const Header = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   const { pathname } = useLocation();
   const headerRef = useRef(null);
   const active = headerNav.findIndex((e) => e.path === pathname);
   const activeStyle = "text-red-600 transition duration-150 ease-in";
-
   return (
     <div
+      className="fixed z-10 flex justify-between  items-center h-24 w-full max-w-[1240px] mx-auto px-4 text-white"
       ref={headerRef}
-      className="fixed top-0 z-20 left-0  px-4 md:px-14 py-4 flex items-center justify-between"
     >
-      <div className="top-0 left-2 flex items-center">
-        <Link className="" to="/">
-          <div className="flex items-center space-x-2">
-            <RiMovie2Fill className="text-red-700 h-9 w-9" />
-            <h3 className="text-white font-semibold">tMovies</h3>
-          </div>
+      <div className="">
+        <Link to="/">
+          <RiMovie2Fill className="text-red-700 h-9 w-9" />
         </Link>
       </div>
-      <ul className="fixed right-5 sm:space-x-6 md:space-x-16 hidden sm:inline-flex sm: ">
+      <ul className=" hidden sm:flex items-center justify-end w-1/2 space-x-10">
         {headerNav.map((e, i) => (
           <li
             key={i}
             className={`font-medium text-white ${
+              i === active ? activeStyle : ""
+            } hover:text-red-600 transition duration-150 ease-in-out`}
+          >
+            <Link to={e.path}>{e.display}</Link>
+          </li>
+        ))}
+      </ul>
+      <div onClick={handleNav} className="block sm:hidden">
+        {nav ? (
+          <RiCloseFill className="text-white" size={20} />
+        ) : (
+          <FcMenu className="text-white" size={20} />
+        )}
+      </div>
+      <ul
+        className={
+          nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#081b27] md:hidden ease-in-out duration-500"
+            : "ease-in-out duration-500 fixed left-[-100%]"
+        }
+      >
+        <Link to="/">
+          <RiMovie2Fill className="text-red-700 m-2 h-9 w-9" />
+        </Link>
+        {headerNav.map((e, i) => (
+          <li
+            key={i}
+            className={`font-medium mt-5 text-white border-b p-3 ${
               i === active ? activeStyle : ""
             } hover:text-red-600 transition duration-150 ease-in-out`}
           >
